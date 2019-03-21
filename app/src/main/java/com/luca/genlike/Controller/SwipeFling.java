7adc912b33ff74d6f517adc51ae2f0e94eca0f5e
luca genco <la172275@student.helha.be>
2019-03-21 11:36:37 +0100

    Ajout du swipe

---
+++ app/src/main/java/com/luca/genlike/Controller/SwipeFling.java
@@ -0,0 +1,53 @@
+package com.luca.genlike.Controller;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.luca.genlike.MainActivity;
+
+import java.util.ArrayList;
+
+public class SwipeFling implements SwipeFlingAdapterView.onFlingListener {
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private MainActivity mainActivity;
+
+    private int i;
+
+    public SwipeFling(ArrayList<String> arrayList, ArrayAdapter<String> arrayAdapter, MainActivity context){
+        mainActivity = context;
+        this.al = arrayList;
+        this.arrayAdapter = arrayAdapter;
+        i = 0;
+    }
+
+    @Override
+    public void removeFirstObjectInAdapter() {
+        Log.d("LIST", "removed object!");
+        al.remove(0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLeftCardExit(Object o) {
+        Toast.makeText(mainActivity, "left", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRightCardExit(Object o) {
+        Toast.makeText(mainActivity, "right", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAdapterAboutToEmpty(int i) {
+        al.add("XML ".concat(String.valueOf(i)));
+        arrayAdapter.notifyDataSetChanged();
+        Log.d("LIST", "notified");
+        i++;
+    }
+
+    @Override
+    public void onScroll(float v) {
+
+    }
+}
--- app/src/main/java/com/luca/genlike/MainActivity.java
+++ app/src/main/java/com/luca/genlike/MainActivity.java
@@ -2,12 +2,51 @@ package com.luca.genlike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.luca.genlike.Controller.SwipeFling;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    SwipeFlingAdapterView flingContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        al = new ArrayList<>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al);
+
+        flingContainer = findViewById(R.id.frame);
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFling(al, arrayAdapter, this));
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
